d64dfcfdcfc8e59beace164a7b842f4a40cb0d2b
aoshima-yuki <aoshima-yuki@ipa.go.jp>
2026-04-15 18:03:08 +0900

    Update start-http.sh

--- docker/start-http.sh
+++ docker/start-http.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-sed -i '/^nifi.web.https.port=/d' /opt/nifi/nifi-current/conf/nifi.properties
-sed -i '/^nifi.web.https.host=/d' /opt/nifi/nifi-current/conf/nifi.properties
+PROP_FILE="/opt/nifi/nifi-current/conf/nifi.properties"
 
-echo "nifi.web.http.host=0.0.0.0" >> /opt/nifi/nifi-current/conf/nifi.properties
-echo "nifi.web.http.port=8080" >> /opt/nifi/nifi-current/conf/nifi.properties
+# HTTPS設定を削除
+sed -i '/^nifi.web.https.port=/d' "$PROP_FILE"
+sed -i '/^nifi.web.https.host=/d' "$PROP_FILE"
+
+# HTTP設定も一旦削除してから入れ直す
+sed -i '/^nifi.web.http.host=/d' "$PROP_FILE"
+sed -i '/^nifi.web.http.port=/d' "$PROP_FILE"
+
+echo "nifi.web.http.host=0.0.0.0" >> "$PROP_FILE"
+echo "nifi.web.http.port=8080" >> "$PROP_FILE"
 
 cd /opt/nifi/nifi-current
 exec ../scripts/start.sh
