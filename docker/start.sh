f9027e0fad6735c9f379acd7354f284e2172e151
aoshima-yuki <aoshima-yuki@ipa.go.jp>
2026-04-15 17:29:09 +0900

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# HTTPS無効化
 sed -i '/^nifi.web.https.port=/d' /opt/nifi/nifi-current/conf/nifi.properties
 sed -i '/^nifi.web.https.host=/d' /opt/nifi/nifi-current/conf/nifi.properties
 
-# HTTP設定
 echo "nifi.web.http.host=0.0.0.0" >> /opt/nifi/nifi-current/conf/nifi.properties
 echo "nifi.web.http.port=8080" >> /opt/nifi/nifi-current/conf/nifi.properties
 
-# 元の起動スクリプト実行
-/opt/nifi/scripts/start.sh
+cd /opt/nifi/nifi-current
+exec ../scripts/start.sh
